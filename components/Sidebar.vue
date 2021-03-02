11c8b0222f9c6729b80eca56253c14944fad2591
Vanka <vanka@iMac-Skogarev.local>
2021-03-02 11:12:07 +0200

    remover sidebar && some fix && added horizontal nav

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,27 +1,49 @@
 <template>
-  <div>
-    <b-sidebar
-      title="Money $"
-      bg-variant="dark"
-      text-variant="light"
-      shadow
-      visible
-      no-header-close
-    >
-      <div class="px-3 py-2">
-        <nav>
-          <div class="link">
-            <nuxt-link to="/">Dashboard</nuxt-link>
-          </div>
-          <div class="link">
-            <nuxt-link to="/">История</nuxt-link>
-          </div>
-          <div class="link">
-            <nuxt-link to="/">Профиль</nuxt-link>
-          </div>
-        </nav>
-      </div>
-    </b-sidebar>
+  <div class="container-fluid primary-bg-color">
+    <div class="nav-wrapper">
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input
+                size="sm"
+                class="mr-sm-2"
+                placeholder="Search"
+              ></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                >Search</b-button
+              >
+            </b-nav-form>
+
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
   </div>
 </template>
 
@@ -44,5 +66,12 @@ nav {
       background-color: rgba(0, 0, 0, 0.1);
     }
   }
+}
+.primary-bg-color {
+  background-color: #16a2b8;
+}
+.nav-wrapper {
+  max-width: 1140px;
+  margin: 0 auto;
 }
 </style>
